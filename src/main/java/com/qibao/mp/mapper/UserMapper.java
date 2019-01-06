474909b40b0bb6461161ba4976080012c10a640d
e <1127090290@qq.com>
2019-01-07 00:26:05 +0800

    commit

---
+++ src/main/java/com/qibao/mp/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.qibao.mp.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qibao.mp.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
--- src/test/java/com/qibao/mp/MpApplicationTests.java
+++ src/test/java/com/qibao/mp/MpApplicationTests.java
@@ -1,16 +1,24 @@
 package com.qibao.mp;
 
+import com.qibao.mp.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MpApplicationTests {
+    @Autowired
+    private UserMapper  userMapper;
 
     @Test
     public void contextLoads() {
+        List userlist =  userMapper.selectList(null);
+        System.out.println(userlist.toString());
     }
 
 }
