b0345b735aad03c6b53ed56ceed551ecb148ba8b
e <1127090290@qq.com>
2019-01-07 00:15:18 +0800

    first commit

---
+++ src/main/java/com/qibao/mp/MpApplication.java
@@ -0,0 +1,14 @@
+package com.qibao.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpApplication.class, args);
+    }
+
+}
+
